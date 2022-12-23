3bd5d2341324e91f7bba3b5e36fc31868c07fed2
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-12-23 20:33:31 +0200

    add env and db_init

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
-const DB_HOST =
-  "mongodb+srv://Artur:5090610@cluster0.6jhcm3f.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-mongoose.set("strictQuery", true);
-
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log("Database connection successful"))
-  .catch((error) => console.log(error));
 
 const controllers = require("../../controllers/contacts");
 
