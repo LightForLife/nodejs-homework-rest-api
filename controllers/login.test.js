2fa22f2e21f85332107fd479ac0b5c7232f13429
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-01-14 23:04:38 +0200

    fix login

--- controllers/login.test.js
+++ controllers/login.test.js
@@ -9,19 +9,22 @@ describe("test login controllers", (done) => {
     server = app.listen(3000);
   });
 
-  test("signup have status 200", async () => {
-    const response = await request(app).post("/api/users/signup").send({
-      email: "testSignUp@example.com",
-      password: "3333333",
-    });
-    // expect(response.status).toBe(200);
-    console.log(response.status);
-    console.log(response.body);
-  });
-
   afterAll(() => {
     console.log("Выполнить после тестов");
-
     server.close();
+  });
+
+  test("signup have status 200", async () => {
+    const response = await request(app)
+      .post("/api/users/login")
+      .send({
+        email: "supertest3@example.com",
+        password: "333333",
+      })
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    console.log(response.status);
+    console.log(response.body);
   });
 });
