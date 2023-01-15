50f3d20dafe2798b4fccdbdbdd01723fe44782d2
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-01-15 16:53:30 +0200

    add full success test

--- controllers/login.test.js
+++ controllers/login.test.js
@@ -1,30 +1,54 @@
 const express = require("express");
 const request = require("supertest");
+const mongoose = require("mongoose");
 
 const app = require("../app");
+const { DB_HOST } = process.env;
 
-describe("test login controllers", (done) => {
+mongoose.set("strictQuery", true);
+
+describe("test login controller", () => {
   beforeAll(() => {
-    console.log("Выполнить в начале тестов");
-    server = app.listen(3000);
+    // console.log("Run before tests");
+    mongoose.connect(DB_HOST);
   });
 
   afterAll(() => {
-    console.log("Выполнить после тестов");
-    server.close();
+    // console.log("Run after tests");
+    mongoose.connection.close();
   });
 
-  test("signup have status 200", async () => {
+  test("response must have status code 200, should return a token, should return a user object with 2 fields email and subscription, both of data type String", async () => {
     const response = await request(app)
       .post("/api/users/login")
       .send({
-        email: "supertest3@example.com",
-        password: "333333",
+        email: "supertest5@example.com",
+        password: "555555",
       })
       .set("Accept", "application/json");
 
+    const { token } = response.body;
+
     expect(response.status).toBe(200);
-    console.log(response.status);
-    console.log(response.body);
+
+    expect(token).toBeDefined();
+
+    expect(response.body).toMatchObject({
+      token: expect.any(String),
+      user: {
+        email: expect.any(String),
+        subscription: expect.any(String),
+      },
+    });
+
+    // expect(response.body).toHaveProperty("user", {
+    //   email: "supertest5@example.com",
+    //   subscription: "starter",
+    // });
+
+    // expect(response.body.user).toEqual({
+    //   email: "supertest5@example.com",
+    //   subscription: "starter",
+    // });
   });
 });
